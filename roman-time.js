1010b53973b61d9dbc2e8b2dec0aa32c6f96153b
CMblCL <victor7053@gmail.com>
2015-10-10 14:48:27 +0500

    решение

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,62 @@
+function ReplaceNumber(number) {
+    var result = "";
+
+    if (number == 0) {
+        return "—";
+    }
+
+    if (number >= 50) {
+        result += "L";
+        number -= 50;
+    }
+
+    if (number >= 40) {
+        result += "XL";
+        number -= 40;
+    }
+
+    for (var i = 0; i < 3; i++) {
+        if (number >= 10) {
+            result += "X";
+            number -= 10;    
+        }
+    }
+
+    switch (number) {
+        case 1: result += "I"; break;
+        case 2: result += "II"; break;
+        case 3: result += "III"; break;
+        case 4: result += "IV"; break;
+        case 5: result += "V"; break;
+        case 6: result += "VI"; break;
+        case 7: result += "VII"; break;
+        case 8: result += "VIII"; break;
+        case 9: result += "IX"; break;
+    }
+
+    return result;
+}
+
+
+
 var hours = process.argv[2];
 var minutes = process.argv[3];
+var errorMessage = "Время указано не верно"; 
 
-// Немного замечательного кода и магии
+hours = Number(hours);
+minutes = Number(minutes);
+
+if (isNaN(hours) || isNaN(minutes)) {
+    console.log(errorMessage);
+    return;
+}
+
+if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
+    console.log(errorMessage);
+    return;
+}
+
+hours = ReplaceNumber(hours);
+minutes = ReplaceNumber(minutes);
+
+console.log(hours + ":" + minutes);
